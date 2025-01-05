4cba4050a36bf9ea0521dd8a78001dbe5ecd1318
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-01-05 11:49:53 +0530

    adding usercontroller

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const authRoutes = require("./routers/authRoutes");
-
+const userRoutes = require("./routers/userRoutes");
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +12,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
