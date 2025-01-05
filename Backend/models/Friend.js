1d7ef7e92c67266ef4f42974161b1801a1b17db6
subhashprasad2001@gmail.com <{96110162+subhashdippu@users.noreply.github.com}+{subhashdippu}@users.noreply.github.com>
2025-01-05 09:08:03 +0530

    adding friend model

---
+++ Backend/models/Friend.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const friendSchema = new mongoose.Schema({
+  requester: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  status: {
+    type: String,
+    enum: ["pending", "accepted", "rejected"],
+    default: "pending",
+  },
+});
+
+module.exports = mongoose.model("Friend", friendSchema);
